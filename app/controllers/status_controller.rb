17880a1af246f2057b7d5a5d68f17a2835573442
Tuan Nguyen <amtuannguyen@gmail.com>
2023-05-15 14:43:06 -0400

    order by last ping DESC

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -15,23 +15,23 @@ class StatusController < ApplicationController
 
     case params[:status]
     when "in_use"
-      @computers = Computer.attached_to_location.in_use
+      @computers = Computer.attached_to_location.in_use.order('last_ping DESC');
     when "not_in_use"
-      @computers = Computer.attached_to_location.not_in_use
+      @computers = Computer.attached_to_location.not_in_use.order('last_ping DESC');
     when "pinging"
-      @computers = Computer.attached_to_location.pinging
+      @computers = Computer.attached_to_location.pinging.order('last_ping DESC');
     when "not_pinging"
-      @computers = Computer.attached_to_location.not_pinging
+      @computers = Computer.attached_to_location.not_pinging.order('last_ping DESC');
     when "staying_active"
-      @computers = Computer.attached_to_location.staying_active
+      @computers = Computer.attached_to_location.staying_active.order('last_ping DESC');
     when "not_staying_active"
-      @computers = Computer.attached_to_location.not_staying_active
+      @computers = Computer.attached_to_location.not_staying_active.order('last_ping DESC');
     when "never_ping"
-      @computers = Computer.attached_to_location.never_ping
+      @computers = Computer.attached_to_location.never_ping.order('last_ping DESC');
     when "never_used"
-      @computers = Computer.attached_to_location.never_used
+      @computers = Computer.attached_to_location.never_used.order('last_ping DESC');
     else
-      @computers = Computer.attached_to_location.in_use
+      @computers = Computer.attached_to_location.in_use.order('last_ping DESC');
     end
 
   end
